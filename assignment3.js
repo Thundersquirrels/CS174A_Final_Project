e01412e83c4a2de954e28db616b44995479a7729
ArvinDing <dingarvin@gmail.com>
2023-05-23 21:49:30 -0700

    totoro

--- assignment3.js
+++ assignment3.js
@@ -5,6 +5,42 @@ const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture
 } = tiny;
 
+
+class Totoro extends Shape {
+    // **Cube** A closed 3D shape, and the first example of a compound shape (a Shape constructed
+    // out of other Shapes).  A cube inserts six Square strips into its own arrays, using six
+    // different matrices as offsets for each square.
+    constructor() {
+        super("position", "normal", "texture_coord");
+        // // Loop 3 times (for each axis), and inside loop twice (for opposing cube sides):
+        // for (let i = 0; i < 2; i++)
+        //     for (let j = 0; j < 2; j++) {
+        //         const square_transform = Mat4.rotation(i == 0 ? Math.PI / 2 : 0, 1, 0, 0)
+        //             .times(Mat4.rotation(Math.PI * j - (i == 1 ? Math.PI / 2 : 0), 0, 1, 0))
+        //             .times(Mat4.translation(0, 0, 1));
+        //         // Calling this function of a Square (or any Shape) copies it into the specified
+        //         // Shape (this one) at the specified matrix offset (square_transform):
+        //         defs.Square.insert_transformed_copy_into(this, [], square_transform);
+        //     }
+        
+        //body
+        defs.Subdivision_Sphere.insert_transformed_copy_into(this, [3], Mat4.scale(3,4,3));
+        //head
+        defs.Subdivision_Sphere.insert_transformed_copy_into(this, [3], Mat4.translation(0,3,0).times(Mat4.scale(2,2,2)));
+        //ears
+        defs.Subdivision_Sphere.insert_transformed_copy_into(this, [3], Mat4.rotation(25,0,0,1).times(Mat4.translation(0,5,0).times(Mat4.scale(0.35,1.35,0.35))));
+        defs.Subdivision_Sphere.insert_transformed_copy_into(this, [3], Mat4.rotation(-25,0,0,1).times(Mat4.translation(0,5,0).times(Mat4.scale(0.35,1.35,0.35))));
+        
+        const arm_scale = Mat4.scale(0.6, 2.5, 2);
+        //left arm
+        const left_arm_transform = Mat4.rotation(10,0,0,1).times(Mat4.translation(-3, 2, 0).times(arm_scale));
+        defs.Subdivision_Sphere.insert_transformed_copy_into(this, [3], left_arm_transform);
+        // right arm
+        const right_arm_transform = Mat4.rotation(-10,0,0,1).times(Mat4.translation(3, 2, 0).times(arm_scale));
+        defs.Subdivision_Sphere.insert_transformed_copy_into(this, [3], right_arm_transform);
+    }
+}
+
 export class Assignment3 extends Simulation {
     // ** Inertia_Demo** demonstration: This scene lets random initial momentums
     // carry several bodies until they fall due to gravity and bounce.
@@ -13,9 +49,11 @@ export class Assignment3 extends Simulation {
         this.shapes = {
             cylinder: new defs.Capped_Cylinder(12, 12, [[0, 5], [0, 1]]),
             square: new defs.Square(),
+            totoro: new Totoro() // Add the Totoro shape to the shapes object
         }
         const shader = new defs.Fake_Bump_Map(1);
         this.material = new Material(shader, {color: color(.1, .9, .9, 1), ambient: .4})
+
     }
 
     update_state(dt) {
@@ -53,5 +91,9 @@ export class Assignment3 extends Simulation {
         this.shapes.square.draw(context, program_state, Mat4.translation(0, -10, 0)
                 .times(Mat4.rotation(Math.PI / 2, 1, 0, 0)).times(Mat4.scale(50, 50, 1)),
             this.material.override(color(.1, .8, .6, 1)));
+
+        this.shapes.totoro.draw(context, program_state, Mat4.identity(), this.material);
+
     }
 }
+
